4dd97b1bb048ef2209aa38fcf4a36354e00a6af4
Kenny Lim <kennylimkokteong@hotmail.com>
2017-11-03 09:04:45 +0800

    Initial Commit

---
+++ Week3Assessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week3Assessment
+//
+//  Created by Kenny Lim on 11/3/17.
+//  Copyright © 2017 Kenny Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
